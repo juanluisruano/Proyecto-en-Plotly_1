7df1f448f37dbbc7d62f61e406ed9af1225fcea1
Juan Ruano <aplicacionesjuanr@gmail.com>
2019-01-18 19:51:26 +0100

    Add files via upload

---
+++ server2.js
@@ -0,0 +1,40 @@
+var serialport = require('serialport'),
+    plotly = require('plotly')('Plotly_UserName','Plotly_API'),
+    token = 'Plotly_Token';
+
+var portName = '/dev/tty.usbmodem1411';
+var sp = new serialport.SerialPort(portName,{
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+    parser: serialport.parsers.readline("\r\n")
+});
+
+function getDateString() {
+    var time = new Date().getTime();
+    // para tu zona horaria solo multiplica +/-GMT by 36000000
+    var datestr = new Date(time +32400000).toISOString().replace(/T/, ' ').replace(/Z/, '');
+    return datestr;
+}
+
+var initdata = [{x:[], y:[], stream:{token:token, maxpoints: 500}}];
+var initlayout = {fileopt : "extend", filename : "sensor-test"};
+
+plotly.plot(initdata, initlayout, function (err, msg) {
+    if (err) return console.log(err)
+
+    console.log(msg);
+    var stream = plotly.stream(token, function (err, res) {
+        console.log(err, res);
+    });
+
+    sp.on('data', function(input) {
+        if(isNaN(input) || input > 1023) return;
+
+    var streamObject = JSON.stringify({ x : getDateString(), y : input });
+    console.log(streamObject);
+    stream.write(streamObject+'\n');
+    });
+});
